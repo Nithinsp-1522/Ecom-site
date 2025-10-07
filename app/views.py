9048fec15e35bb07d370da4655b453e865230ef2
Nishanth SP <spbrothers4@gmail.com>
2025-10-07 21:51:45 +0530

    Initial commit

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'user/index.html')
 
 
 # Create your views here.
