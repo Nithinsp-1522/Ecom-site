e95553f43faf9ca4f7dba37d92b8867029923d99
Nithinsp-1522 <spbrothers4@gmail.com>
2025-10-08 17:27:12 +0530

    Updated product model and fixed UI issue

--- app/urls.py
+++ app/urls.py
@@ -5,10 +5,20 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('userlogin/', views.userlogin, name='userlogin'),
+    path('userlogout/', views.userlogout, name='userlogout'),
     
-    
+    # User URLs
+    path('profile/', views.profile, name='profile'),
+    path('address/', views.address, name='address'),
+    path('order-details/', views.order_details, name='order-details'),
+    path('payment-method/', views.payment_method, name='payment-method'),
+    path('rewards/', views.rewards, name='rewards'),
+
+
+
     # Admin URLs can be added here
     path('admin-home/', views.admin_home, name='admin-home'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,16 +2,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'user/index.html')
+    return render(request, 'index.html')
 
 def about(request):
-    return render(request, 'user/about.html')
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
 
 def signup(request):
-    return render(request, 'user/signup.html')  
+    return render(request, 'signup.html')  
 
-def login(request):
-    return render(request, 'user/signin.html')
+def userlogin(request):
+    return render(request, 'signin.html')
+
+def userlogout(request):
+    return render(request, 'index.html')
+
+
+# user views
+def profile(request):
+    return render(request, 'user/account-settings.html')
+
+def address(request):
+    return render(request, 'user/account-address.html')
+
+def order_details(request):
+    return render(request, 'user/account-orders.html')
+
+def payment_method(request):
+    return render(request, 'user/account-payment-method.html')
+
+def rewards(request):
+    return render(request, 'user/account-notification.html')
+
 
 
 
@@ -19,4 +44,4 @@ def login(request):
 
 # Admin views
 def admin_home(request):
-    return render(request, 'admin/adminhome.html')
+    return render(request, 'superadmin/adminhome.html')
